feb9e7a6fe897ae7cefcdd2cd95880ade35b4d1c
bhara <bhara@Lisa>
2025-12-17 20:34:40 +0530

    operators

--- Tnsproject/src/project/Operators.java
+++ Tnsproject/src/project/Operators.java
@@ -17,7 +17,6 @@ System.out.println("d value after the operations: " + d);
 
 		System.out.println("a , b,c,d values after the operations: " + a + " " + b + " " + c + " " + d);
 				
-		//Ternary operator demonstration
 		  x = (10 == x) ? 1 : 0;	
 		  System.out.println(x);
 	}
